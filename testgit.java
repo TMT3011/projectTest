28b306bac94c8ed5d71ac1fbf1fb48350694af6a
TMT3011 <tranminhtu1130@gmail.com>
2026-03-19 22:47:56 +0700

    update data

--- testgit.java
+++ testgit.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class testgit{
-    public static void main(String a[]){
+    public static void main(String arg[]){
         System.out.println("Hello GIT");
+        int a, b;
+        Scanner kb=new Scanner(System.in);
+        System.out.println("Nhap vao a:");
+        a=kb.nextInt();
+        System.out.println("Nhap vao b:");
+        b=kb.nextInt();
+        System.out.println("Tong a va b la: "+(a+b));
     }
 }
